9002a6dcdfc4e93f6eb966830b91b3e0191797ee
YishayGarame <yishaygr@gmail.com>
2021-04-24 23:42:15 +0300

    spaces comments

--- scanner.cpp
+++ scanner.cpp
@@ -3,13 +3,70 @@
 
 shared_ptr<Token> Scanner::nextToken()
 {
-    while(nextChar())
+    //read the first char of the file
+    nextChar();
+
+ // Case: ' ' (Space)
+    if(ch == ' ' || ch == '\n') //rrl
     {
-        switch(ch)
+        while(nextChar())
         {
-            case ' ' :
-            brea
+            if(ch != ' ' || ch != '\n')
+            {
+                inputFile.unget();
+                break;
+            }
         }
     }
+
+
+    // Case: // || /**/
+    if(ch == '/')
+    {
+        int currentLine = lineno;
+        nextChar();
+        if(ch == '/')  //  "//"
+        {
+            while(nextChar())
+            {
+                if(currentLine != lineno) 
+                {
+                    inputFile.unget();
+                    break;
+                }
+            }
+        }
+
+        //case /**/
+        if(ch == '*')
+        {
+            while(nextChar())
+            {
+                if(ch == '*' )
+                {
+                    nextChar(); // in case * is  solid
+                    if(ch == '/') // */
+                    {
+                     break;
+                    }
+                }
+            }
+        }
+  
+    }
+
+    switch (ch) {
+// each character represents itself
+case ';' : case '{' : case '}' : case ',' : case ':' : \
+case '(' : case ')' : case '[' : case ']' : case '~' : \
+case '*' : case '%' : case '^' : case '?' : case '/':
+return shared_ptr<Token>
+(new Token(static_cast<tokenType>(ch),string(1,ch)));
+break;
+
+        
+
     return shared_ptr<Token>();
 }
+}
+
